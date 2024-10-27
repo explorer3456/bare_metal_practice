bfc96b9292682f7e0e712f3bdf2c051da57cece2
Ditto Kim <rlawhdgus34@gmail.com>
2024-10-27 13:50:30 +0900

    initial stm32 startup code
    - startup code should contains vector table, which is not should be located in data section.
    - we want vector table should be located in user defined section, vector

---
+++ hello_world/scratch/stm32_startup.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+
+#define SRAM_START	(0x20000000U)
+#define SRAM_SIZE	(128 * 1024U) // 128 KB
+#define SRAM_END	((SRAM_START) + (SRAM_SIZE))
+
+#define STACK_START	SRAM_END
+
+void Reset_Handler(void);
+
+uint32_t vectors[] = {
+	STACK_START, // MSP
+	(uint32_t *)Reset_Handler,
+};
+
+void Reset_Handler(void)
+{
+}
